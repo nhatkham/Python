bfcc630fc89343a195150f835f00bc0d58a3d375
nhatkham <nhatkham@gmail.com>
2023-08-18 14:16:43 +0700

    a

---
+++ Hello.py
@@ -0,0 +1,11 @@
+import requests
+
+def check_internet():
+    url='http://www.google.com/'
+    timeout=5
+    try:
+        _ = requests.get(url, timeout=timeout)
+        return True
+    except requests.ConnectionError:
+        print("No internet connection available.")
+    return False
